6367fd5719c98d978803c607a820b6c1e6a1c284
MilesMartinez <milesmartinez@outlook.com>
2025-06-29 00:12:14 -0500

    Implement dynamic basePath for image URLs in galleries and update next.config.js for production environment

--- next.config.js
+++ next.config.js
@@ -1,10 +1,17 @@
 /** @type {import('next').NextConfig} */
+const isProduction = process.env.NODE_ENV === 'production';
+const basePath = isProduction ? '/patchwork' : '';
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   output: 'export',
+  basePath,
   images: {
     unoptimized: true,
+  },
+  env: {
+    basePath: basePath, // Make basePath available to the app
   }
 }
 
